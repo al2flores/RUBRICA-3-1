c4056b214913f16f3c4df1bf2a0b48129e07b1fa
Al2 <Al2@DESKTOP-J6VTO31>
2016-08-25 16:45:36 -0500

    Prueba  #5  programa para sacar el factorial de un numero

---
+++ RUBRICA-3-1/src/rubrica/pkg3/pkg1/Clase5.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rubrica.pkg3.pkg1;
+
+/**
+ *
+ * @author Al2
+ */
+public class Clase5 {
+
+    public static void main(String args[]) {
+
+        for (int counter = 0; counter <= 10; counter++){
+
+            System.out.printf("%d! = %d\n", counter,
+
+            factorial(counter));
+
+        }
+
+    }
+
+    public static long factorial(long number) {
+
+        if (number <= 1)
+
+            return 1;
+
+        else
+
+            return number * factorial(number - 1);
+
+    }
+
+}  
