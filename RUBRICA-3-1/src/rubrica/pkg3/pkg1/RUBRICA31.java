dccbe0adcacf2fb7eb271f5a7b56305b0f14cea0
Al2 <Al2@DESKTOP-J6VTO31>
2016-08-25 10:24:14 -0500

    Prueba  #1

---
+++ RUBRICA-3-1/src/rubrica/pkg3/pkg1/RUBRICA31.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rubrica.pkg3.pkg1;
+
+/**
+ *
+ * @author Al2
+ */
+public class RUBRICA31 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola 5to B");
+    }
+    
+}
